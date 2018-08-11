dab204bdabcc6f8857b89192aced46c47f9e0bd2
sotir3d <sebastian.fueloep@gmail.com>
2018-08-11 15:45:17 +0200

    Alles kopiert >:)

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    //public GameObject feet;
+    //public AudioClip footstepSound;
+
+    Vector2 direction;
+    Quaternion playerRotation;
+    Rigidbody2D playerRigidbody;
+
+    //Animator anim;
+    //Animator feetAnim;
+
+    //AudioSource playerAudioSource;
+
+    float angle;
+    float horizontalInput;
+    float verticalInput;
+
+    float lastFootstepSound;
+    float footstepSoundSpeed = 0.6f;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerRotation = transform.rotation;
+        playerRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ignores input when game is paused
+        if (Time.timeScale == 0)
+            return;
+
+        playerRigidbody.velocity = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * moveSpeed;
+
+        direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+
+        angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        playerRotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = playerRotation;
+    }
+}
