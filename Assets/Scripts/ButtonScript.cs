44ea6c511d2ad799320e79f39b681eeacb0293a5
sotir3d <sebastian.fueloep@gmail.com>
2018-08-13 18:48:58 +0200

    Button click sounds

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonScript : MonoBehaviour, IPointerEnterHandler, IPointerDownHandler
+{
+    AudioSource audioSource;
+    public AudioClip hoverSound;
+
+    // Use this for initialization
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        audioSource.PlayOneShot(hoverSound);
+    }
+}
